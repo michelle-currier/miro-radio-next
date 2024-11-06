f307fcad099db49443a519d348290a14c40c689c
michelle-currier <mushel@gmail.com>
2024-11-06 01:02:30 -0600

    adding aria labels to iframes

--- components/sound-player.tsx
+++ components/sound-player.tsx
@@ -57,6 +57,7 @@ export default function SoundPlayer() {
         className="rounded-md"
         width="100%"
         height="166"
+        aria-label="Soundcloud playlist for Miro Radio"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1901635067&color=%236840c7&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
 
--- components/video-player.tsx
+++ components/video-player.tsx
@@ -15,6 +15,7 @@ const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId }) => (
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
+      aria-label="Youtube playlist for Miro Radio "
     />
   </div>
 );
